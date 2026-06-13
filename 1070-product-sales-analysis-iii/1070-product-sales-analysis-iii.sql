baf642331ef14fa81ea220a9901b407f15a73ed8
Ramina Ibraimova <romiisromie@gmail.com>
2026-06-13 21:32:19 +0500

    Time: 739 ms (44.04%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,16 @@
+WITH RankedSales AS (
+    SELECT 
+        product_id,
+        year AS first_year,
+        quantity,
+        price,
+        RANK() OVER(PARTITION BY product_id ORDER BY year ASC) AS rnk
+    FROM Sales
+)
+SELECT 
+    product_id, 
+    first_year, 
+    quantity, 
+    price
+FROM RankedSales
+WHERE rnk = 1;
