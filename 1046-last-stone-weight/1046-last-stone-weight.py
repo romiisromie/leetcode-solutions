81c36b0024ed0c8cab77e1553d957450d6872879
Ramina Ibraimova <romiisromie@gmail.com>
2026-06-21 19:59:38 +0500

    Time: 1 ms (41.32%), Space: 12.5 MB (16.64%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        while len(stones) > 1:
+            stones.sort()
+            stone1 = stones.pop()
+            stone2 = stones.pop()
+            if stone1 != stone2:
+                stones.append(stone1 - stone2)
+        return stones[0] if stones else 0
