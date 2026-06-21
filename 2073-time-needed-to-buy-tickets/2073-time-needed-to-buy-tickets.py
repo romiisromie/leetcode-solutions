06e9bd7252c97ad2a2a980f46a04660ea8f16a65
Ramina Ibraimova <romiisromie@gmail.com>
2026-06-21 12:36:50 +0500

    Time: 0 ms (100%), Space: 12.3 MB (90.05%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def timeRequiredToBuy(self, tickets, k):
+        target = tickets[k]
+        time = 0
+        
+        for i in range(len(tickets)):
+            if i <= k:
+                time += min(tickets[i], target)
+            else:
+                time += min(tickets[i], target - 1)
+                
+        return time
