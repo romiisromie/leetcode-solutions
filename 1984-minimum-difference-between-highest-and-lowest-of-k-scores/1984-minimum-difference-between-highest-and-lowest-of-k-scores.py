1860136bd750fc8d87d162856c2c44e7816ea996
Ramina Ibraimova <romiisromie@gmail.com>
2026-06-22 12:36:33 +0500

    Time: 4 ms (93.92%), Space: 12.6 MB (31%) - LeetHub

---
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minimumDifference(self, nums, k):
+        if k == 1:
+            return 0
+            
+        nums.sort()
+        min_diff = float('inf')
+        
+        for i in range(len(nums) - k + 1):
+            diff = nums[i + k - 1] - nums[i]
+            if diff < min_diff:
+                min_diff = diff
+                
+        return min_diff
