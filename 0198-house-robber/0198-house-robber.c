1c4d6cbb88b41d2ebde46f880ef35116e79ba3cb
Ramina Ibraimova <romiisromie@gmail.com>
2026-06-25 11:16:38 +0500

    Time: 0 ms (100%), Space: 8.8 MB (68.18%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+int rob(int* nums, int numsSize) {
+    if (numsSize == 0) return 0;
+    if (numsSize == 1) return nums[0];
+
+    int prev2 = 0;
+    int prev1 = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        int current = prev2 + nums[i] > prev1 ? prev2 + nums[i] : prev1;
+        prev2 = prev1;
+        prev1 = current;
+    }
+
+    return prev1;
+}
