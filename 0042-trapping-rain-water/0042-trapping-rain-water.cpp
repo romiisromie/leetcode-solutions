57eb63214a83bc4c7afa860985e5d13ade0dec70
Ramina Ibraimova <romiisromie@gmail.com>
2026-06-26 23:04:05 +0500

    Time: 0 ms (100%), Space: 25.8 MB (93.42%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int trap(std::vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int left_max = 0;
+        int right_max = 0;
+        int water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= left_max) {
+                    left_max = height[left];
+                } else {
+                    water += left_max - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= right_max) {
+                    right_max = height[right];
+                } else {
+                    water += right_max - height[right];
+                }
+                right--;
+            }
+        }
+
+        return water;
+    }
+};
