348eebc0a55f70b56f4ac8da23946d44d08b9abe
Ramina Ibraimova <romiisromie@gmail.com>
2026-06-26 22:49:59 +0500

    Time: 3 ms (37.74%), Space: 40.1 MB (95.81%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> productExceptSelf(std::vector<int>& nums) {
+        int n = nums.size();
+        std::vector<int> answer(n, 1);
+
+        for (int i = 1; i < n; ++i) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            answer[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return answer;
+    }
+};
