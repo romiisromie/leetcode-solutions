d1ed602f0b56feb872f6158b7543575917d29a54
Ramina Ibraimova <romiisromie@gmail.com>
2026-07-03 19:17:54 +0500

    Time: 10 ms (26.27%), Space: 14 MB (70.69%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows <= 1 || s.length() <= numRows) return s;
+
+        vector<string> rows(numRows);
+        int currRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[currRow] += c;
+            if (currRow == 0 || currRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currRow += goingDown ? 1 : -1;
+        }
+
+        string result;
+        for (const string& row : rows) {
+            result += row;
+        }
+
+        return result;
+    }
+};
