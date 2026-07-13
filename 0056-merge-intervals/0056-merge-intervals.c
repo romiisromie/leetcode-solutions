c3beaf330bd33557c9d3d56503812512f5ee52d5
Ramina Ibraimova <romiisromie@gmail.com>
2026-07-13 13:43:30 +0500

    Time: 7 ms (19.82%), Space: 18.6 MB (74.41%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.c
+++ 0056-merge-intervals/0056-merge-intervals.c
@@ -3,7 +3,10 @@
 int compareIntervals(const void* a, const void* b) {
     int* intervalA = *(int**)a;
     int* intervalB = *(int**)b;
-    return intervalA[0] - intervalB[0];
+    if (intervalA[0] != intervalB[0]) {
+        return intervalA[0] - intervalB[0];
+    }
+    return intervalA[1] - intervalB[1];
 }
 
 int** merge(int** intervals, int intervalsSize, int* intervalsColSize, int* returnSize, int** returnColumnSizes) {
@@ -16,30 +19,28 @@ int** merge(int** intervals, int intervalsSize, int* intervalsColSize, int* retu
     qsort(intervals, intervalsSize, sizeof(int*), compareIntervals);
 
     int** merged = (int**)malloc(intervalsSize * sizeof(int*));
-    merged[0] = (int*)malloc(2 * sizeof(int));
-    merged[0][0] = intervals[0][0];
-    merged[0][1] = intervals[0][1];
-    
-    int count = 1;
+    *returnColumnSizes = (int*)malloc(intervalsSize * sizeof(int));
 
-    for (int i = 1; i < intervalsSize; i++) {
-        if (intervals[i][0] <= merged[count - 1][1]) {
-            if (intervals[i][1] > merged[count - 1][1]) {
-                merged[count - 1][1] = intervals[i][1];
+    int count = 0;
+    merged[count] = (int*)malloc(2 * sizeof(int));
+    merged[count][0] = intervals[0][0];
+    merged[count][1] = intervals[0][1];
+    (*returnColumnSizes)[count] = 2;
+
+    for (int i = 1; i < intervalsSize; ++i) {
+        if (intervals[i][0] <= merged[count][1]) {
+            if (intervals[i][1] > merged[count][1]) {
+                merged[count][1] = intervals[i][1];
             }
         } else {
+            count++;
             merged[count] = (int*)malloc(2 * sizeof(int));
             merged[count][0] = intervals[i][0];
             merged[count][1] = intervals[i][1];
-            count++;
+            (*returnColumnSizes)[count] = 2;
         }
     }
 
-    *returnSize = count;
-    *returnColumnSizes = (int*)malloc(count * sizeof(int));
-    for (int i = 0; i < count; i++) {
-        (*returnColumnSizes)[i] = 2;
-    }
-
+    *returnSize = count + 1;
     return merged;
 }
