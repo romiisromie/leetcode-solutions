e3cda179fea120ad49076ab730f82b00149a0616
Ramina Ibraimova <romiisromie@gmail.com>
2026-07-13 13:57:30 +0500

    Time: 0 ms (100.00%) | Memory: 13.5 MB (34.19%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0;
+        int twos = 0;
+        
+        for (int num : nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+        
+        return ones;
+    }
+};
