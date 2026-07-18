a9c89fc313763ee6a8e2689044efd42dc4144939
Ramina Ibraimova <romiisromie@gmail.com>
2026-07-18 10:32:59 +0500

    Time: 0 ms (100%), Space: 2.2 MB (40.07%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.rs
@@ -0,0 +1,31 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut low = 0;
+        let mut high = nums.len() as i32 - 1;
+
+        while low <= high {
+            let mid = low + (high - low) / 2;
+            let mid_val = nums[mid as usize];
+
+            if mid_val == target {
+                return mid;
+            }
+
+            if nums[low as usize] <= mid_val {
+                if nums[low as usize] <= target && target < mid_val {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if mid_val < target && target <= nums[high as usize] {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        -1
+    }
+}
